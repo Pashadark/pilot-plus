52af34f326f7886715a0f4469fb0a27d102c091e
pasahdark <sedoffwork@ya.ru>
2026-07-29 08:35:37 +0300

    fix: allow local network dev access

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   reactCompiler: true,
+  allowedDevOrigins: ['127.0.0.1', '192.168.31.172'],
   images: {
     remotePatterns: [
       {
